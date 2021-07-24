bf1ef3e4bfeb41c42c57605997b4a0c04236abf7
Alphashow <lastblast@outlook.fr>
2021-07-24 12:38:59 +0100

    Initial commit

---
+++ Tetris-Raylib/Main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(1280, 720, "Tetris");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
